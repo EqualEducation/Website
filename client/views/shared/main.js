7133337f13500c382430155b1e6543de8a9b613b
Carla Goldstein <carla.goldstein@gmail.com>
2015-06-29 15:34:51 +0200

    added method to check for logged in user's email address

--- client/views/shared/main.js
+++ client/views/shared/main.js
@@ -4,3 +4,10 @@
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     //$('.collapsible').collapsible();
   });
+
+
+  Template.registerHelper("userEmailAddress", function (param2) {
+    var user = Meteor.user();
+    if (user && user.emails)
+        return user.emails[0].address;
+});
