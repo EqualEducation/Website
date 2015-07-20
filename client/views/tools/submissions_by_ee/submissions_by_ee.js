08f837b099799a771597567c42e2989fe42803b6
unknown <cece.marela@gmail.com>
2015-07-20 16:19:49 +0200

    I validated the submit button in the subbmision_by_ee page to only show when a user is logged in. I added a function that allows the save button of the submit modal to actually save in the database. I added the SubmissionByEe collection in the lib/collection.js file to make it accessible to both the client and the server.

---
+++ client/views/tools/submissions_by_ee/submissions_by_ee.js
@@ -0,0 +1,52 @@
+Template.submissions_by_ee.onRendered(function() {
+    $('.modal-trigger').leanModal();
+})
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("submissionsByEe", function () {
+    return SubmissionByEe.find();
+  });
+}
+
+if(Meteor.isClient){
+  // This code only runs on the client
+  Meteor.subscribe("submissionsByEe");
+
+  Template.submissions_by_ee.helpers({
+    submissionsByEe: function () {
+      SubmissionByEe.find().fetch();
+    }
+
+
+  });
+
+  Template.submissions_by_ee.events({
+
+    "click .save" : function(event, template ){
+      //event.preventDefault();
+      //var pdfFile  = template.find('.pdfFile').value;
+      var descript = template.find('.description').value;
+      console.log(descript);
+      console.log("insert");
+      SubmissionByEe.insert({
+        description: descript,
+        createdBy : Meteor.userId()
+      });
+
+    console.log(Meteor.userId());
+      //Meteor.call("addFile", description);
+
+      //return false;
+    }
+
+  });
+}
+  // Meteor.methods({
+  //   addFile : function(description){
+  //     console.log("insert called");
+  //       SubmissionByEe.insert({
+  //         description: description
+  //       });
+  //     }
+  //
+  // });
