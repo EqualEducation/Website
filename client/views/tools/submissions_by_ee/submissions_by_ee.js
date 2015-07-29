11455d42f7524c9abb35bbd2081dca6f3128828a
unknown <cece.marela@gmail.com>
2015-07-29 15:32:46 +0200

    Added filepicker plus. Uploading files to filepicker server working.

--- client/views/tools/submissions_by_ee/submissions_by_ee.js
+++ client/views/tools/submissions_by_ee/submissions_by_ee.js
@@ -1,11 +1,59 @@
-Template.submissions_by_ee.onRendered(function() {
+ Template.submissions_by_ee.onRendered(function() {
     $('.modal-trigger').leanModal();
-})
+    //filepicker.constructWidget(document.getElementById('upload'));
+ })
 
 if(Meteor.isClient){
-  // This code only runs on the client
-  Meteor.subscribe("submissionsByEe");
 
+  Meteor.subscribe("fileUploads");
+  Template.submissions_by_ee.helpers({
+    theFiles: function () {
+      return YourFileCollection.find();
+    }
+  });
+
+  Template.submissions_by_ee.events({
+
+    'click #upload-file': function (evt) {
+      evt.preventDefault();
+      console.log('showing')
+      var self = this;
+          filepicker.pick( function onSuccess(Blob){
+            console.log(Blob.url);
+            console.log("file picked");
+          });
+
+      // filepicker.pick(
+      //     function(Blobs){
+      //       console.log(Blob.url);
+      //     }
+      // );
+    },
+
+    'click #deleteFileButton ': function (event) {
+      console.log("deleteFile button ", this);
+      YourFileCollection.remove({_id: this._id});
+    },
+    'change .your-upload-class': function (event, template) {
+      console.log("uploading...")
+      FS.Utility.eachFile(event, function (file) {
+        console.log("each file...");
+        var yourFile = new FS.File(file);
+        console.log(yourFile.url());
+        YourFileCollection.insert(yourFile, function (err, fileObj) {
+          console.log("callback for the insert, err: ", err);
+          if (!err) {
+            console.log("inserted without error");
+          }
+          else {
+            console.log("there was an error", err);
+          }
+        });
+      });
+    }
+  });
+
+  Meteor.subscribe("submissionsByEe");
   Template.submissions_by_ee.helpers({
     submissionsByEe: function () {
       return SubmissionByEe.find().fetch();
@@ -17,30 +65,17 @@ if(Meteor.isClient){
   Template.submissions_by_ee.events({
 
     "click .save" : function(event, template ){
-      //event.preventDefault();
-      //var pdfFile  = template.find('.pdfFile').value;
+
       var descript = template.find('.description').value;
-      console.log(descript);
-      console.log("insert");
       SubmissionByEe.insert({
         description: descript,
         createdBy : Meteor.userId()
       });
-      console.log(SubmissionByEe.find());
-    console.log(Meteor.userId());
-      //Meteor.call("addFile", description);
-
-      //return false;
+      FlashMessages.sendSuccess("Successfully saved");
     }
-
   });
+
+
+
+
 }
-  // Meteor.methods({
-  //   addFile : function(description){
-  //     console.log("insert called");
-  //       SubmissionByEe.insert({
-  //         description: description
-  //       });
-  //     }
-  //
-  // });
