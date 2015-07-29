72f6c3df013fb5f1087ec3b5ca7cd05650b552b3
unknown <cece.marela@gmail.com>
2015-07-29 16:23:10 +0200

    added the url value to the submissionsbyee collection

--- client/views/tools/submissions_by_ee/submissions_by_ee.js
+++ client/views/tools/submissions_by_ee/submissions_by_ee.js
@@ -1,57 +1,8 @@
  Template.submissions_by_ee.onRendered(function() {
     $('.modal-trigger').leanModal();
-    //filepicker.constructWidget(document.getElementById('upload'));
  })
 
 if(Meteor.isClient){
-
-  Meteor.subscribe("fileUploads");
-  Template.submissions_by_ee.helpers({
-    theFiles: function () {
-      return YourFileCollection.find();
-    }
-  });
-
-  Template.submissions_by_ee.events({
-
-    'click #upload-file': function (evt) {
-      evt.preventDefault();
-      console.log('showing')
-      var self = this;
-          filepicker.pick( function onSuccess(Blob){
-            console.log(Blob.url);
-            console.log("file picked");
-          });
-
-      // filepicker.pick(
-      //     function(Blobs){
-      //       console.log(Blob.url);
-      //     }
-      // );
-    },
-
-    'click #deleteFileButton ': function (event) {
-      console.log("deleteFile button ", this);
-      YourFileCollection.remove({_id: this._id});
-    },
-    'change .your-upload-class': function (event, template) {
-      console.log("uploading...")
-      FS.Utility.eachFile(event, function (file) {
-        console.log("each file...");
-        var yourFile = new FS.File(file);
-        console.log(yourFile.url());
-        YourFileCollection.insert(yourFile, function (err, fileObj) {
-          console.log("callback for the insert, err: ", err);
-          if (!err) {
-            console.log("inserted without error");
-          }
-          else {
-            console.log("there was an error", err);
-          }
-        });
-      });
-    }
-  });
 
   Meteor.subscribe("submissionsByEe");
   Template.submissions_by_ee.helpers({
@@ -64,12 +15,28 @@ if(Meteor.isClient){
 
   Template.submissions_by_ee.events({
 
-    "click .save" : function(event, template ){
+
+
+    'click #upload-file': function (evt) {
+      evt.preventDefault();
+      console.log('showing')
+      var self = this;
+          filepicker.pick( function onSuccess(Blob){
+            console.log(Blob.url);
+            console.log("file picked");
+            $('.file-url').val(Blob.url);
+          });
+
+    },
+
+    'click .save' : function(event, template ){
 
       var descript = template.find('.description').value;
+      var fileUrl = template.find('.file-url').value;
       SubmissionByEe.insert({
         description: descript,
-        createdBy : Meteor.userId()
+        createdBy : Meteor.userId(),
+        fileUrl: fileUrl
       });
       FlashMessages.sendSuccess("Successfully saved");
     }
